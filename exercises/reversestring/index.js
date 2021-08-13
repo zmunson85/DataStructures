f11f1a9ff2fa6174a43497112791a580f458d817
Zach Munson <zmunson85@gmail.com>
2021-08-13 12:08:25 -0600

    ReverseString

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -11,30 +11,36 @@
     initiate newStr variable
     loop each char in the str, return newStr value. 
 */
-function reverse(str) {
-    let newStr = '';
+// function reverse(str) {
+//     let newStr = '';
 
-    for (let character of str) {
-        newStr = character + newStr;
-    }
+//     for (let character of str) {
+//         newStr = character + newStr;
+//     }
 
-    return newStr;
-}
+//     return newStr;
+// }
 
-module.exports = reverse;
+// module.exports = reverse;
 
 /*
 Solution 2:
 Refactored with built in JS methods */
-// function reverse(str) {
-//     return str
-//         .split('')              //split method splits a string into an array of substrings, then returns that array
-//         .reverse()          //reverse method will reverse the order of elements in the array
-//         .join('');             //join will return an array as a string
-//     //self returns the original str in reverse order, split, reverse and join/return
-// }
+function reverse(str) {
+    return str
+        .split('')
+        .reverse()
+        .join('');
+}
+module.exports = reverse;
+/*  NOTES:
+.split('') split method splits string into an array of substrings, then returns that array.
+.reverse() reverse method reverses the order of elements in an array.
+.join('') join method will return an array as a string.
+*/
 
-// module.exports = reverse;
+
+
 //Refactor BigO of 1
 /* create a function that will return a reversed value of the string given */
 // function reverse(str) {
@@ -46,8 +52,3 @@ Refactored with built in JS methods */
 // module.exports = reverse;
 
 
-/*  NOTES:
-.split('') split method splits string into an array of substrings, then returns that array.
-.reverse() reverse method reverses the order of elements in an array.
-.join('') join method will return an array as a string.
-*/
